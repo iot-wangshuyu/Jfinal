cc8b722655c59709c37f9806021eb370662129a2
shuyu.wang <iot_wangshuyu@126.com>
2017-10-10 11:12:58 +0800

    实现数据库查询，分页等功能

--- src/jfinal/demo/HelloController.java
+++ src/jfinal/demo/HelloController.java
@@ -8,6 +8,7 @@ import com.jfinal.core.JFinal;
 import com.jfinal.ext.interceptor.POST;
 import com.jfinal.json.FastJson;
 import com.jfinal.kit.HttpKit;
+import com.jfinal.plugin.activerecord.Page;
 
 import jfinal.demo.bean.User;
 import jfinal.demo.dao.UserDao;
@@ -45,6 +46,24 @@ public class HelloController extends Controller {
 		boolean save = new UserDao().set("name", "wang").save();
 		List<UserDao> find = new UserDao().find("select name,pass_word from t_user");
 		renderText("执行结果："+find);
+	}
+	
+	public void queryOne() {
+		UserDao user=UserDao.dao.findFirst("select name,pass_word from t_user");
+		renderJson(user);
+	}
+	
+	public void queryList() {
+		List<UserDao> find = UserDao.dao.find("select name,pass_word from t_user");
+		renderJson(find);
+	}
+	
+	
+	public void queryPage() {
+		Integer pageNumber = getParaToInt("pageNumber");
+		Integer pageSize = getParaToInt("pageSize");
+		Page<UserDao> paginate = UserDao.dao.paginate(pageNumber, pageSize, "select name,pass_word"," from t_user");
+		renderJson(paginate);
 	}
 	
 	public static void main(String[] args) {
